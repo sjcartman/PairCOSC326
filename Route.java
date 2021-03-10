43742930a68875ecd23ae6e0a11f0e70457a7636
staycosc326 <80072978+staycosc326@users.noreply.github.com>
2021-03-10 22:45:38 +1300

    Add files via upload

--- Route.java
+++ Route.java
@@ -1,34 +1,68 @@
-public class Route{
+/**
+ * Stores and displays information about the word chains
+ */
+
+public class Route {
     private String value;
     private String target;
     private int hops;
     private boolean hopsSet;
 
-    public Route (String inValue, String inTarget){
+    /** sets the value of data field to input parameter value with no int input
+     * @param inValue value of the word
+     * @param inTarget target of the word
+     */
+    public Route (String inValue, String inTarget) {
         
         value = inValue;
         target = inTarget;
         hopsSet = false;
     }
 
-    public Route(String inValue,String inTarget, int inHops){
+      /** sets the value of data field to input parameter value with int input
+     * @param inValue value of the word
+     * @param inTarget target of the word
+     * @param inHops hops of the integer value
+     */
+
+    public Route(String inValue,String inTarget, int inHops) {
         value = inValue;
         target = inTarget;
         hopsSet = true;
         hops = inHops;
     }
-
-    public String getValue(){
+    
+    /**
+     * returns the value of the data field value
+     * @return the value of the word
+     */
+    public String getValue() {
         return value;
     }
 
-    public String getTarget(){
+    /** 
+     * returns the value of the data field target
+     * @return target word
+     */ 
+
+    public String getTarget() {
         return target;
     }
+
+    /**
+     * returns the value of the data field hops
+     * @return the number of hops
+     */
+    
     public int getHops(){
         return hops;
     }
-    public boolean isHopsSet(){
+
+    /**
+     * returns a boolean value of the data field hopsSet
+     * @return true or false
+     */
+    public boolean isHopsSet() {
         return hopsSet;
     }
 
