ebe0f90be101071e973de638e4de4316815233d4
staycosc326 <80072978+staycosc326@users.noreply.github.com>
2021-03-10 22:46:09 +1300

    Add files via upload

--- GraphNode.java
+++ GraphNode.java
@@ -1,34 +1,34 @@
 /**
- *@author Sean Cartman
+ * @author Sean Cartman
  * A graph node containing links all its neighbours and its value(string)
  */
 import java.util.*;
 
-public class GraphNode{
+public class GraphNode {
     private String value;
     private ArrayList<GraphNode> neighbours = new ArrayList<GraphNode>();
     private boolean neighboursSet = false;
 
-    public void setNeighboursSet(){
+    public void setNeighboursSet() {
         neighboursSet = true;
     }
 
-    public boolean getNeighboursSet(){
+    public boolean getNeighboursSet() {
         return neighboursSet;
     }
 
-    public GraphNode(String input){
+    public GraphNode(String input) {
         value = input;
     }
-    public void addNeighbour (GraphNode n){
+    public void addNeighbour (GraphNode n) {
         neighbours.add(n);
     }
-    public GraphNode[] getNeighbours(){
+    public GraphNode[] getNeighbours() {
         GraphNode[] out = new GraphNode[neighbours.size()];
         out = neighbours.toArray(out);
         return out;
     }
-    public String getWord(){
+    public String getWord() {
         return value;
     }
 
