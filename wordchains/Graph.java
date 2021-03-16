a3b8364afe8339eec8b994cc906f1a1f34be050c
Sean Cartman <casea836@student.otago.ac.nz>
2021-03-16 13:00:34 +1300

    s

--- wordchains/Graph.java
+++ wordchains/Graph.java
@@ -9,104 +9,101 @@ package wordchains;
 import java.util.*;
 
 
- public class Graph {
-     // declaration for input
-    private static ArrayList<GraphNode> nodes = new ArrayList<GraphNode>();
-     // delcaration for the chain
-    private static ArrayList<Route> chains = new ArrayList<Route>();
+public class Graph {
+    // declaration for input
+   private static ArrayList<GraphNode> nodes = new ArrayList<GraphNode>();
+    // delcaration for the chain
+   private static ArrayList<Route> chains = new ArrayList<Route>();
+  
 
+   private static void link() {
+       for(GraphNode i :nodes){
+           if (!i.getNeighboursSet()) {
+               for (GraphNode j:nodes){
+           // convert string to character
+                   int totalDifferences = 0;
+                   char[] jChars = j.getWord().toCharArray();
+                   char[] iChars = i.getWord().toCharArray();
+                   if (iChars.length == jChars.length) {
+                       for (int k = 0; k < jChars.length;k++) {
+                           if (jChars[k] != iChars[k]) {
+                               totalDifferences++;
+                           }
+                       }
+           // Add to counter if there is a change in character
+                       if (totalDifferences == 1) {
+                           i.addNeighbour(j);
+                       }   
+                   }
+                   
+               }
+           }
+           i.setNeighboursSet();
+       }
+   }
 
-   
+    /**
+     * Gets the input from stdin
+     */
 
-    private static void link() {
-        for(GraphNode i :nodes){
-            if (!i.getNeighboursSet()) {
-                for (GraphNode j:nodes){
-		    // convert string to character
-                    int totalDifferences = 0;
-                    char[] jChars = j.getWord().toCharArray();
-                    char[] iChars = i.getWord().toCharArray();
-                    if (iChars.length == jChars.length) {
-                        for (int k = 0; k < jChars.length;k++) {
-                            if (jChars[k] != iChars[k]) {
-                                totalDifferences++;
-                            }
-                        }
-			// Add to counter if there is a change in character
-                        if (totalDifferences == 1) {
-                            i.addNeighbour(j);
-                        }   
-                    }
-                    
-                }
-            }
-            i.setNeighboursSet();
-        }
-    }
-
-     /**
-      * Gets the input from stdin
-      */
-
-    private static void getInput() {
-        boolean readingWordList = false;
-        Scanner scan = new Scanner(System.in);
-        while (scan.hasNextLine()) {
-            String line = scan.nextLine();
-            if (line.length() == 0 && readingWordList) {
-                System.err.println("Empty Line");
-                return;
-            }
-            
-            else if (line.length() == 0) {
-                readingWordList = true;
-            }
-            else if (!readingWordList) {
-                try{
-                 
-                    Scanner scanLine = new Scanner(line);
-                    String word1 = scanLine.next(); 
-                    String word2 = scanLine.next();
-                    if(scanLine.hasNextInt()) {
-                        int i = scanLine.nextInt();
-                        chains.add(new Route(word1,word2,i));
-                    }
-                    else {
-                        chains.add(new Route(word1,word2));
-                    }
-                    scanLine.close();
+   private static void getInput() {
+       boolean readingWordList = false;
+       Scanner scan = new Scanner(System.in);
+       while (scan.hasNextLine()) {
+           String line = scan.nextLine();
+           if (line.length() == 0 && readingWordList) {
+               System.err.println("Empty Line");
+               return;
+           }
+           
+           else if (line.length() == 0) {
+               readingWordList = true;
+           }
+           else if (!readingWordList) {
+               try{
                 
-                }catch(Exception e){
-                    System.err.println("Invalid input: "+line);
-                }
-            }
-            
-            else if (readingWordList) {
-                Scanner scanLine = new Scanner(line);
-                try {
-                    String s = scanLine.next();
-                    boolean set = true; ;
-                    for(GraphNode n:nodes){
-                        if(n.getWord().equals(s)){
-                        System.err.println("Invalid input: "+line);
-                        set = false;
-                        }
-                    }
-                    if(set){
-                        nodes.add(new GraphNode(s));
-                    }
-                
-                } catch (Exception e) {
-                    System.err.println("Invalid input: "+line);
-                }
-                scanLine.close();
+                   Scanner scanLine = new Scanner(line);
+                   String word1 = scanLine.next(); 
+                   String word2 = scanLine.next();
+                   if(scanLine.hasNextInt()) {
+                       int i = scanLine.nextInt();
+                       chains.add(new Route(word1,word2,i));
+                   }
+                   else {
+                       chains.add(new Route(word1,word2));
+                   }
+                   scanLine.close();
                
-            }
+               }catch(Exception e){
+                   System.err.println("Invalid input: "+line);
+               }
+           }
+           
+           else if (readingWordList) {
+               Scanner scanLine = new Scanner(line);
+               try {
+                   String s = scanLine.next();
+                   boolean set = true; ;
+                   for(GraphNode n:nodes){
+                       if(n.getWord().equals(s)){
+                       System.err.println("Invalid input: "+line);
+                       set = false;
+                       }
+                   }
+                   if(set){
+                       nodes.add(new GraphNode(s));
+                   }
+               
+               } catch (Exception e) {
+                   System.err.println("Invalid input: "+line);
+               }
+               scanLine.close();
+              
+           }
 
-        }
-        scan.close();
-    }
-    
+       }
+       scan.close();
+   }
 
      /**
       * Using breath first search to connect the word chains
